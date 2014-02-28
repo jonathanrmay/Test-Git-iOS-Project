aca7ee6c2490d63928a946be953050b24e67adf0
mayjr <jonathan@Jonathans-MacBook-Pro-2.local>
2014-02-27 23:40:25 -0500

    added a test class

---
+++ JRMTest.h
@@ -0,0 +1,13 @@
+//
+//  JRMTest.h
+//  Git Test
+//
+//  Created by mayjr on 2/27/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JRMTest : NSObject
+
+@end
---
+++ JRMTest.m
@@ -0,0 +1,13 @@
+//
+//  JRMTest.m
+//  Git Test
+//
+//  Created by mayjr on 2/27/14.
+//
+//
+
+#import "JRMTest.h"
+
+@implementation JRMTest
+
+@end
